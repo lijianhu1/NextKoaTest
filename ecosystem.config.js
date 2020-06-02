3a1d30dee3673a4dd4b4701ccc2b9050b34d0f00
lijianhu1 <403056352@qq.com>
2020-06-02 11:37:20 +0800

    修改至服务器测试

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,59 +18,11 @@ module.exports = {
         RUNTIME_ENV: 'dev',
         PORT: 6016,
         PROTOCOL: 'http',
-        HOST: 'http://dev.mypsb.cn',
-        DOMAIN: 'dev.mypsb.cn',
-        RedisHost: 'dev.mypsb.cn',
+        HOST: '119.23.50.196',
+        DOMAIN: '119.23.50.196',
+        RedisHost: '119.23.50.196',
         RedisPort: 6379,
         RedisPwd: 'Redis_VanvyPsb2018'
-      }
-    },
-    {
-      name: 'PSB_TEST',
-      script: './server.js',
-      time: true,
-      args: 'one two',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'development'
-      },
-      env_test: {
-        NODE_ENV: 'production',
-        RUNTIME_ENV: 'test',
-        PORT: 6017,
-        PROTOCOL: 'http',
-        HOST: 'http://test.mypsb.cn',
-        DOMAIN: 'test.mypsb.cn',
-        RedisHost: 'test.mypsb.cn',
-        RedisPort: 6380,
-        RedisPwd: 'Redis_VanvyPsb2018'
-      }
-    },
-    {
-      name: 'PSB',
-      script: './server.js',
-      time: true,
-      args: 'one two',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'development'
-      },
-      env_prod: {
-        NODE_ENV: 'production',
-        RUNTIME_ENV: 'prod',
-        PORT: 6060,
-        PROTOCOL: 'https',
-        HOST: 'https://mypsb.cn',
-        DOMAIN: 'mypsb.cn',
-        RedisHost: 'redis-ddbb608-redis.dcs.huaweicloud.com',
-        RedisPort: 6379,
-        RedisPwd: 'redis_eip_product_pass_important'
       }
     }
   ],
@@ -78,21 +30,12 @@ module.exports = {
   deploy: {
     dev: {
       user: 'root',
-      host: 'dev.mypsb.cn',
+      host: '119.23.50.196',
       ref: 'origin/master',
-      repo: 'git@git.vanvy.cn:chenfuye/NextPsb.git',
+      repo: 'git@github.com:lijianhu1/NextKoaTest.git',
       path: '/root/NextPsb',
       'post-deploy':
         'npm install && npm run build && pm2 reload ecosystem.config.js --only PSB_DEV --env dev'
-    },
-    test: {
-      user: 'root',
-      host: 'test.mypsb.cn',
-      ref: 'origin/master',
-      repo: 'git@git.vanvy.cn:chenfuye/NextPsb.git',
-      path: '/data/eip/test/NextPsb',
-      'post-deploy':
-        'npm install && npm run build && pm2 reload ecosystem.config.js --only PSB_TEST --env test'
     }
   }
 }
